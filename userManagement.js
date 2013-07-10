2e4651668209f0528f40c9ec85c0b9b6618d309a
Rachel Scalfani <rscalfani@gmail.com>
2013-07-10 15:06:23 -0700

    code review and cleanup removed debugging code validated confirm parameters moved saltHash and corresponding components to authenticator

--- userManagement.js
+++ userManagement.js
@@ -1,4 +1,3 @@
-var crypto = require('crypto');
 var emailConfig = require('./emailConfig');
 var emailer = require('./emailer').newEmailer(emailConfig);
 var sqlUtils = require('./sqlUtils');
@@ -6,10 +5,7 @@ var url = require('url');
 var uuid = require('node-uuid');
 
 var userManagement = {
-	newUserManager: function(validation, validator, authenticator, client) {
-		var saltHash = function(password, salt, cb) {
-			crypto.pbkdf2(password, salt, 10000, 64, cb);
-		};
+	newUserManager: function(validation, validator, authenticator, dbClient) {
 		var manager = {
 			login: function(req, res) {
 				var validationDef = {
@@ -24,25 +20,23 @@ var userManagement = {
 					res.redirect('/html/error.html?err=' + JSON.stringify(errors));
 					return;
 				}
-				console.log(req.body);
 
 				var cmd = "" +
 					"SELECT user_id \n" +
 					"FROM users \n" +
 					"WHERE email = " + sqlUtils.quote(req.body.email);
-				console.log(cmd);
 
-				client.query(cmd, function(err, result) {
+				dbClient.query(cmd, function(err, result) {
 					if (err || result.rows.length == 0)
 						res.redirect('/html/error.html?err=notauthenticated');
 					else
 					{
-						saltHash(req.body.password, result.rows[0].user_id, function(err, derivedKey) {
+						var userId = result.rows[0].user_id;
+						authenticator.passwordHash(userId, req.body.password, function(err, passwordHash) {
 							if (err)
-								console.log(err);
+								res.redirect('/html/error.html?err=notauthenticated');
 							else
 							{
-								var passwordHash = derivedKey.toString('hex');
 								authenticator.authenticate(req.body.email, passwordHash, function(authenticated) {
 									if (authenticated)
 										res.redirect('http://skavenger.com.ar/homerswebpage/');
@@ -79,58 +73,52 @@ var userManagement = {
 					res.redirect('/html/error.html?err=' + JSON.stringify(errors));
 					return;
 				}
-				else
-				{
-					var userId = uuid.v4();
-					saltHash(req.body.password, userId, function(err, derivedKey) {
-						if (err)
-							console.log(err);
-						else
-						{
-							var passwordHash = derivedKey.toString('hex');
-							var confirmationId = uuid.v4();
-							var cmd = "" +
-								"INSERT INTO users (first_name, middle_name, last_name, email, password, sign_up_ts, confirmation_id, user_id) \n" +
-								"VALUES (" + sqlUtils.quote(req.body.firstName) + ", " + sqlUtils.quote(req.body.middleName) + ", " + sqlUtils.quote(req.body.lastName) + ", " + sqlUtils.quote(req.body.email) + ", " + sqlUtils.quote(passwordHash) + ", CURRENT_TIMESTAMP AT TIME ZONE 'UTC', " + sqlUtils.quote(confirmationId) + "," + sqlUtils.quote(userId) + ")";
-							console.log(cmd);
-							client.query(cmd, function(err, result) {
-								if (err)
-									res.redirect('/html/error.html?err=' + err);
-								else
+				// create random userId
+				var userId = uuid.v4();
+				authenticator.passwordHash(userId, req.body.password, function(err, passwordHash) {
+					if (err)
+						res.redirect('/html/error.html?err=' + err);
+					else
+					{
+						// create random confirmation id for email
+						var confirmationId = uuid.v4();
+						var cmd = "" +
+							"INSERT INTO users (first_name, middle_name, last_name, email, password, sign_up_ts, confirmation_id, user_id) \n" +
+							"VALUES (" + sqlUtils.quote(req.body.firstName) + ", " + sqlUtils.quote(req.body.middleName) + ", " + sqlUtils.quote(req.body.lastName) + ", " + sqlUtils.quote(req.body.email) + ", " + sqlUtils.quote(passwordHash) + ", CURRENT_TIMESTAMP AT TIME ZONE 'UTC', " + sqlUtils.quote(confirmationId) + "," + sqlUtils.quote(userId) + ")";
+						dbClient.query(cmd, function(err, result) {
+							if (err)
+								res.redirect('/html/error.html?err=' + err);
+							else
+							{
+								if (result.rowCount == 1)
 								{
-									console.log(result);
-									if (result.rowCount == 1)
-									{
-										var urlParts = url.parse(req.headers.origin);
-										console.log(urlParts);
-										urlParts.pathname = '/confirm';
-										urlParts.search = 'confirmationId=' + confirmationId;
-										var confirmationLink = url.format(urlParts);
-										emailer.send({
-											from: '<rscalfani@gmail.com>',
-											to: '<' + req.body.email + '>',
-											subject: 'Welcome to Maiden, ' + req.body.firstName,
-											html: '<p>Welcome to Maiden</p></br>Please click this link to confirm your email: <a href="' + confirmationLink + '">'+ confirmationLink + '</a>'
-										}, function(err, response){
-											if(err)
-												console.log(err);
-											else
-												console.log("Message sent: " + response.message);
-										});
-										res.redirect('/html/login.html');
-									}
-									else
-										res.redirect('/html/error.html?err=doesnotexist');
+									// send confirmation email
+									var urlParts = url.parse(req.headers.origin);
+									urlParts.pathname = '/confirm';
+									urlParts.search = 'confirmationId=' + confirmationId;
+									var confirmationLink = url.format(urlParts);
+									emailer.send({
+										from: '<rscalfani@gmail.com>',
+										to: '<' + req.body.email + '>',
+										subject: 'Welcome to Maiden, ' + req.body.firstName,
+										html: '<p>Welcome to Maiden</p></br>Please click this link to confirm your email: <a href="' + confirmationLink + '">'+ confirmationLink + '</a>'
+									}, function(err, response){
+										if(err)
+											console.log(err);
+										else
+											console.log("Message sent: " + response.message);
+									});
+									res.redirect('/html/login.html');
 								}
-							});
-						}
-					});
-				}
-				console.log(req.body);
+								else
+									res.redirect('/html/error.html?err=unabletojoin');
+							}
+						});
+					}
+				});
 			},
 			emailCheck: function(req, res) {
 				var ajaxResponse = function(res, o) {
-					console.log(o);
 					res.writeHead(200, { 'Content-Type': 'application/json' });
 					res.write(JSON.stringify(o));
 					res.end();
@@ -153,8 +141,7 @@ var userManagement = {
 					"SELECT * \n" +
 					"FROM users \n" +
 					"WHERE email = " + sqlUtils.quote(req.body.email);
-				console.log(cmd);
-				client.query(cmd, function(err, result) {
+				dbClient.query(cmd, function(err, result) {
 					var success;
 					var exists;
 					var error;
@@ -168,7 +155,6 @@ var userManagement = {
 						success = true;
 						exists = result.rows.length == 1
 					}
-					console.log(result);
 					ajaxResponse(res, {
 						success: success,
 						exists: exists,
@@ -177,24 +163,35 @@ var userManagement = {
 				});
 			},
 			confirm: function(req, res) {
-				var confirmationID = req.query.confirmationId;
+				var validationDef = {
+					confirmationId: {
+						regex: validation.regexes.uuid
+					}
+				};
+				var errors = validator.validate(req.query, validationDef);
+				if (errors)
+				{
+					res.redirect('/html/error.html?err=' + JSON.stringify(errors));
+					return;
+				}
+				var confirmationId = req.query.confirmationId;
 				var cmd = "" +
 					"SELECT * \n" +
 					"FROM users \n" +
-					"WHERE confirmation_id = " + sqlUtils.quote(confirmationID);
-				console.log(cmd);
-				client.query(cmd, function(err, result) {
+					"WHERE confirmation_id = " + sqlUtils.quote(confirmationId);
+				dbClient.query(cmd, function(err, result) {
 					if (err)
 						res.redirect('/html/error.html?err=' + err);
 					else
 					{
 						if (result.rowCount == 1)
 						{
+							// delete confirmation id from user record so user can login
 							cmd = "" +
 								"UPDATE users \n" +
-								"SET confirmation_id = null \n" +
-								"WHERE confirmation_id = " + sqlUtils.quote(confirmationID);
-							client.query(cmd, function (err, result) {
+								"	SET confirmation_id = null \n" +
+								"WHERE confirmation_id = " + sqlUtils.quote(confirmationId);
+							dbClient.query(cmd, function (err, result) {
 								if (err)
 									res.redirect('/html/error.html?err=' + err);
 								else
@@ -202,7 +199,7 @@ var userManagement = {
 							});
 						}
 						else
-							res.redirect('/html/error.html?err=doesnotexist');
+							res.redirect('/html/error.html?err=invalidconfirmationid');
 					}
 				});
 			}
